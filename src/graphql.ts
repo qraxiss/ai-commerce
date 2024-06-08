0f19b4068961b99bf9fc9eeab8f24b547a6d6ec1
qraxiss <qraxiss@gmail.com>
2024-06-08 17:09:51 +0300

    feat: order resolvers connected to main resolvers.

--- src/graphql.ts
+++ src/graphql.ts
@@ -1,7 +1,8 @@
 import cart from "./api/cart/resolvers/cart";
+import payment from "./api/order/resolvers/order";
 
 export async function registerResolvers() {
-  [cart].forEach(async (resolver) => {
+  [cart, payment].forEach(async (resolver) => {
     await strapi.service("plugin::graphql.extension").use(resolver);
   });
 }
