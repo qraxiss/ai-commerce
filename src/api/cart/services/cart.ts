d5052dfaa75f0d413fd2059bf1eaf06d05757b18
Muammer Akca <55638376+muammerakca10@users.noreply.github.com>
2024-06-08 16:16:59 +0300

    feat: cart and order services writing started

--- src/api/cart/services/cart.ts
+++ src/api/cart/services/cart.ts
@@ -3,5 +3,35 @@
  */
 
 import { factories } from '@strapi/strapi';
+import { getId } from '../../../helpers/id';
+const services = ()=>({
+    async addToCart({productId}){
+      const {id} = await strapi.service('api::cart.cart').getUserCart()
+        return strapi.entityService.update('api::cart.cart', id, {
+            data: {
+                items: {
+                    connect: [productId]
+                }
+            }
+        })
 
-export default factories.createCoreService('api::cart.cart');
+    },
+
+    async removeFromCart({productId}){
+        const {id} = await strapi.service('api::cart.cart').getUserCart()
+        return strapi.entityService.update('api::cart.cart', id, {
+            data: {
+                items: {
+                    disconnect: [productId]
+                }
+            }
+        })
+    },
+
+    async getUserCart(){
+        return await strapi.entityService.findOne('api::cart.cart', getId())
+    }
+
+})
+
+export default factories.createCoreService('api::cart.cart', services);
--- src/api/order/services/order.ts
+++ src/api/order/services/order.ts
@@ -3,5 +3,31 @@
  */
 
 import { factories } from '@strapi/strapi';
+import { getId } from '../../../helpers/id';
+
+const services = () => ({
+    async payment(){
+        const {id: cart} = await strapi.service('api::cart.cart').getUserCart()
+
+
+        const order = await strapi.entityService.create('api::order.order', {
+            data: {
+                cart,
+                user:getId()
+            }
+        })
+
+        const newCart = await strapi.entityService.create('api::cart.cart', {
+            data: {
+                user: getId()
+            }
+        })
+
+        return order
+
+
+
+    }
+})
 
 export default factories.createCoreService('api::order.order');
