1980886edacafed1b064e0a43f4ce09e81c87cf1
qraxiss <qraxiss@gmail.com>
2024-06-12 18:12:23 +0300

    feat: makeSuggest route implemented, connected ai service

--- src/api/order/routes/custom-order.ts
+++ src/api/order/routes/custom-order.ts
@@ -5,5 +5,10 @@ export default {
       path: "/order/payment",
       handler: "order.payment",
     },
+    {
+      method: "POST",
+      path: "/order/makeSuggest",
+      handler: "order.makeSuggest",
+    },
   ],
 };
